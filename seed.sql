9fda82536e0f35f5eb3bddb99451080d63d76810
Anthony Moss <asmoss93@gmail.com>
2019-06-26 18:45:42 -0400

    adds seed data and puts tables in correct schema file not the seed

--- seed.sql
+++ seed.sql
@@ -1,14 +1,6 @@
-create table users (
-    id serial PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(50),
-    user_name VARCHAR(50),
-    email VARCHAR(200),
-    password VARCHAR(500)
-);
-
-create table savedGames (
-    id serial PRIMARY KEY,
-    state text,
-    user_id INTEGER REFERENCES users(id)
-);
+insert into users
+    (first_name, last_name, user_name, email, password)
+VALUES
+    ('David', 'Quinith', 'DQ@DQ.com', 'Colombia123!'),
+    ('Jonathan', 'Joestar', 'JoJo@bizadv.com', 'Hamon18'),
+    ('Sterling', 'Archer', 'SecretAgent#1@archer.com', 'Phrasing?')
