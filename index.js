0569074140dbd0b3ffb6e2239d2ad7c3c3bd8811
Anthony Moss <asmoss93@gmail.com>
2019-06-26 18:28:47 -0400

    brings in the save

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require('express');
+const app = express();
+const port = 5000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// const userRouter =  require(route_to_user_router)
+
+// app.use('/heroQuest', userRouter);
+
+app.listen(port, () => {
+    console.log(`you got the backend running on port ${port}`);
+});
