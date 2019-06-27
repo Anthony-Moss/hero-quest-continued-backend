eeaf25fd589ea9ade32a7f00f0d689074a15ecf2
Anthony Moss <asmoss93@gmail.com>
2019-06-26 20:17:37 -0400

    adds utils file with escapeHTML function

---
+++ utils.js
@@ -0,0 +1,12 @@
+function escapeHTML(text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+module.exports = escapeHTML;
