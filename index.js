51e0b0870a95cde38d9e105e11d0e5622109bb15
Anthony Moss <asmoss93@gmail.com>
2019-07-02 01:45:14 -0400

    brings in session and session file store  and new login router

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const path = require('path');
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(
+    session({
+      store: new FileStore(), // no options for now
+      secret: 'abc123'
+    })
+  );
 
 const userRouter =  require('./routers/users')
+const loginRouter = require('./routers/userLoginRoutes')
 
+app.use('/login', loginRouter);
 app.use('/heroQuest', userRouter);
 
 app.listen(port, () => {
